7647647d24788c331f44f4d55e646bcec108c790
Guillaume <g.morlieras@gmail.com>
2022-01-06 15:53:28 +0100

    Initial Commit

---
+++ src/Entity/Voiture.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoitureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoitureRepository::class)]
+class Voiture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Marque;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $modele;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $annee;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->Marque;
+    }
+
+    public function setMarque(string $Marque): self
+    {
+        $this->Marque = $Marque;
+
+        return $this;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(string $modele): self
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    public function getAnnee(): ?string
+    {
+        return $this->annee;
+    }
+
+    public function setAnnee(string $annee): self
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+}
